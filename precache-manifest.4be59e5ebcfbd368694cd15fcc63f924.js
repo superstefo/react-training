89fda444e4b7ad37782c4caaca0e1e5166cabd23
Stefan Stefanov <stefan.stefanov@estafet.com>
2020-01-03 17:37:36 +0200

    Updates

---
+++ precache-manifest.4be59e5ebcfbd368694cd15fcc63f924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7316b74be71fe9ed97c9c11fb6ac23",
+    "url": "/react-training/index.html"
+  },
+  {
+    "revision": "46c3c801e1c24109a9c6",
+    "url": "/react-training/static/css/2.f68ec2e8.chunk.css"
+  },
+  {
+    "revision": "46c3c801e1c24109a9c6",
+    "url": "/react-training/static/js/2.81dcdf6f.chunk.js"
+  },
+  {
+    "revision": "1d4434372b5e2a6e23722a16ff863833",
+    "url": "/react-training/static/js/2.81dcdf6f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05734513dd18aa32ca14",
+    "url": "/react-training/static/js/main.d02be19c.chunk.js"
+  },
+  {
+    "revision": "6dab96c14c2ee5edb051",
+    "url": "/react-training/static/js/runtime-main.3b2fbae6.js"
+  }
+]);
