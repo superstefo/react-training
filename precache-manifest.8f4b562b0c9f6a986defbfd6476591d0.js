0da66d04190d1f0912ef398f1e2073a87822858f
superstefo <elken@abv.bg>
2020-07-14 16:44:43 +0300

    Updates

--- precache-manifest.8f4b562b0c9f6a986defbfd6476591d0.js
+++ precache-manifest.8f4b562b0c9f6a986defbfd6476591d0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d4e827a0e299da8055b2db02c35fbce1",
+    "revision": "cf7ece391dcc37f5728f13c8e66ffd13",
     "url": "/react-training/index.html"
   },
   {
-    "revision": "55e8c0e63d5a41ae4045",
+    "revision": "fa74962d8e89b2405e5d",
     "url": "/react-training/static/css/2.f68ec2e8.chunk.css"
   },
   {
-    "revision": "55e8c0e63d5a41ae4045",
-    "url": "/react-training/static/js/2.e40b82b6.chunk.js"
+    "revision": "fa74962d8e89b2405e5d",
+    "url": "/react-training/static/js/2.45d041ab.chunk.js"
   },
   {
     "revision": "1d4434372b5e2a6e23722a16ff863833",
-    "url": "/react-training/static/js/2.e40b82b6.chunk.js.LICENSE"
+    "url": "/react-training/static/js/2.45d041ab.chunk.js.LICENSE"
   },
   {
-    "revision": "0ea72e822a9876bb1cd3",
-    "url": "/react-training/static/js/main.81d21305.chunk.js"
+    "revision": "922b6e37887fb4722284",
+    "url": "/react-training/static/js/main.0956fc9c.chunk.js"
   },
   {
     "revision": "6dab96c14c2ee5edb051",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-training/precache-manifest.0ea0fe82ed4f9ed03b99efb2b22214c5.js"
+  "/react-training/precache-manifest.8f4b562b0c9f6a986defbfd6476591d0.js"
 );
 
 self.addEventListener('message', (event) => {
