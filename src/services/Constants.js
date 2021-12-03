c5300a467e1f2819380046f321af7667373061d3
Stefan Stefanov <superstefo@google.com>
2021-12-03 11:22:50 +0200

    bump ver 1.55

--- src/services/Constants.js
+++ src/services/Constants.js
@@ -30,7 +30,7 @@ const Const = {
   LOCAL_CASH_VAR_NAME: '__TnDr__',
   AUTH_HEADER_NAME: 'X-Auth-Token',
   PHONE_HEADER_NAME: 'phone-number',
-  VERSION: '1.5'
+  VERSION: '1.55'
 }
 
 export default Const;
